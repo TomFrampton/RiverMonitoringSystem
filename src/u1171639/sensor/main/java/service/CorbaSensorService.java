323bacef3880ee1a5571a885df812b87a2448f8c
TomFrampton <frammo84@hotmail.co.uk>
2015-04-02 14:48:21 +0100

    Added getWaterLevel() method to SensorService interface.

--- src/u1171639/sensor/main/java/service/CorbaSensorService.java
+++ src/u1171639/sensor/main/java/service/CorbaSensorService.java
@@ -2,4 +2,10 @@ package u1171639.sensor.main.java.service;
 
 public class CorbaSensorService implements SensorService {
 
+	@Override
+	public float getWaterLevel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- src/u1171639/sensor/main/java/service/SensorService.java
+++ src/u1171639/sensor/main/java/service/SensorService.java
@@ -1,5 +1,5 @@
 package u1171639.sensor.main.java.service;
 
 public interface SensorService {
-	
+	public float getWaterLevel();
 }
