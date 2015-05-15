bd452c2569d1249c42c6115539ee3533295298dc
TomFrampton <frammo84@hotmail.co.uk>
2015-05-15 11:46:08 +0100

    Cleaned up code.

--- src/u1171639/sensor/main/java/client/CorbaLMS.java
+++ src/u1171639/sensor/main/java/client/CorbaLMS.java
@@ -8,19 +8,26 @@ import u1171639.shared.main.java.exception.ServerNotFoundException;
 import u1171639.shared.main.java.utils.CorbaUtils;
 
 /**
- * CORBA Specific implementation of the LMS client interface. Can connect to a CORBA LMS
- * and raise an alarm.
+ * CORBA Specific implementation of the LMS client. Encapsulates the complexity
+ * of connecting to and communicating with an LMS.
  * @author Tom
  *
  */
 public class CorbaLMS implements LMS {
+	// The servant
 	private LMS_Sensor lms = null;
+	// IOR of the servant
 	private org.omg.CORBA.Object lmsIOR = null;
-	
+	// IOR of the service of this sensor. Will be passed to the LMS when we register this sensor.
 	private String serviceIOR;
-	
+	// Name that the LMS is registered with in the naming service
 	private String servantName;
 	
+	/**
+	 * Constructor.
+	 * @param servantName Name that the LMS is registered with in the naming service
+	 * @param serviceIOR The IOR of this sensors service
+	 */
 	public CorbaLMS(String servantName, String serviceIOR) {
 		this.servantName = servantName;
 		this.serviceIOR = serviceIOR;
@@ -33,13 +40,18 @@ public class CorbaLMS implements LMS {
 	 */
 	@Override
 	public void connect() throws ServerNotFoundException, ConnectionException {
-		try {	
+		try {
+			// If not already connected
 			if(this.lmsIOR == null) {
+				// Find the IOR of the LMS using the naming service
 				this.lmsIOR = CorbaUtils.resolveService(this.servantName);
 			}
 		
+			// Narrow the IOR of the LMS which will open a connection
 			this.lms = LMS_SensorHelper.narrow(this.lmsIOR);
+			// Call the register method of the LMS to register this sensor
 			String sensorName = this.lms.register(this.serviceIOR, SensorConfig.getZone());
+			// LMS will return a sensor name that we will use to identify ourself
 			SensorConfig.setSensorName(sensorName);
 			
 		} catch(org.omg.CORBA.COMM_FAILURE e) {
@@ -60,6 +72,7 @@ public class CorbaLMS implements LMS {
 	 */
 	@Override
 	public void raiseAlarm() {
+		// The zone of this sensor was set in the config file at application startup
 		this.lms.raiseAlarm(SensorConfig.getZone());
 	}
 	
